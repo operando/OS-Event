8bf06056d20be3977b0095a738146bc051ba481e
Shinobu Okano Mac <okanoshinobu8@gmail.com>
2014-07-13 21:12:21 +0900

    add Event the zusaar And atnd_

--- app/controllers/os_event_controller.rb
+++ app/controllers/os_event_controller.rb
@@ -25,6 +25,38 @@ class OsEventController < ApplicationController
 			event_array.push(connpass_event)
 		}
 
+		zusaar_mash = Hashie::Mash.new(JSON.parse(open("http://www.zusaar.com/api/event/user/?user_id=agxzfnp1c2Fhci1ocmRyFgsSBFVzZXIiDDMzMzk4OTM5MV90dww").read))
+
+		zusaar_mash.event.each { |e|
+			zusaar_event = Event.new 
+			
+			zusaar_event.title = e.title
+			zusaar_event.url = e.event_url
+			zusaar_event.address = e.address		
+			zusaar_event.description = e.description
+			zusaar_event.start_time = e.started_at
+			zusaar_event.end_time = e.ended_at
+			zusaar_event.event_site = "zusaar"
+
+			event_array.push(zusaar_event)
+		}
+
+		atnd_mash = Hashie::Mash.new(JSON.parse(open("http://api.atnd.org/events/?nickname=operando&format=json").read))
+
+		atnd_mash.events.each { |e|
+			atnd_event = Event.new 
+			
+			atnd_event.title = e.title
+			atnd_event.url = e.event_url
+			atnd_event.address = e.address		
+			atnd_event.description = e.description
+			atnd_event.start_time = e.started_at
+			atnd_event.end_time = e.ended_at
+			atnd_event.event_site = "atnd"
+
+			event_array.push(atnd_event)
+		}
+
 		render :json => event_array
 	end
 
