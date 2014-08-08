138cba748751d6a826af527f3b2726a37491f119
Shun Sugai <sugaishun@gmail.com>
2014-08-09 02:46:35 +0900

    Fix tests for events#today

--- spec/requests/events_spec.rb
+++ spec/requests/events_spec.rb
@@ -13,18 +13,15 @@ describe 'EventsController' do
     SERVICES = {
       connpass: {
         url: 'http://connpass.com/api/v1/event/?nickname=operandoOS',
-        fixture: 'connpass_event_with_nickname.json',
-        result: 2
+        fixture: 'connpass_event_with_nickname.json'
       },
       zusaar: {
         url: 'http://www.zusaar.com/api/event/?user_id=agxzfnp1c2Fhci1ocmRyFgsSBFVzZXIiDDMzMzk4OTM5MV90dww',
-        fixture: 'zusaar_event_with_id.json',
-        result: 0
+        fixture: 'zusaar_event_with_id.json'
       },
       atnd: {
         url: 'http://api.atnd.org/events/?nickname=operando&format=json',
-        fixture: 'atnd_event_with_nickname.json',
-        result: 5
+        fixture: 'atnd_event_with_nickname.json'
       }
     }
 
@@ -42,33 +39,43 @@ describe 'EventsController' do
       let(:json) { JSON.parse(response.body) }
 
       it 'should return events from today' do
-        expect(json.size).to eq 7
+        expect(json.size).to eq 8
       end
     end
 
-    SERVICES.each do |key, value|
-      describe "GET /event/#{key.to_s}" do
-        before do
-          get "/event/#{key.to_s}", format: :json
-        end
-
-        let(:json) { JSON.parse(response.body) }
-
-        it 'should return events from today' do
-          expect(json.size).to eq value[:result]
-        end
-      end
-    end
-
-    describe 'GET /event/foobarbaz' do
+    describe 'GET /simple' do
       before do
-        get "/event/foobarbaz", format: :json
+        get '/simple', format: :json
       end
 
       let(:json) { JSON.parse(response.body) }
 
-      it 'should return empty array' do
-        expect(json.size).to eq 0
+      it 'should return events from today' do
+        expect(json.size).to eq 8
+      end
+    end
+
+    describe 'GET /week' do
+      before do
+        get '/week', format: :json
+      end
+
+      let(:json) { JSON.parse(response.body) }
+
+      it 'should return events in this week' do
+        expect(json.size).to eq 2
+      end
+    end
+
+    describe 'GET /history' do
+      before do
+        get '/history', format: :json
+      end
+
+      let(:json) { JSON.parse(response.body) }
+
+      it 'should return events before now' do
+        expect(json.size).to eq 21
       end
     end
   end
